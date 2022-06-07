3ba662fcb9442301e5b1d70129fc972202f6c796
AntonioCopete <antcopjim@gmail.com>
2022-06-07 22:13:06 +0200

    feat: first global tests

---
+++ src/__test__/global.test.js
@@ -0,0 +1,53 @@
+const text = "Hola Mundo";
+const fruits = ["manzana", "melon", "banana"];
+
+test("Debe contener un texto", () => {
+  expect(text).toMatch(/Mundo/);
+});
+
+test("¿Tenemos una banana?", () => {
+  expect(fruits).toContain("banana");
+});
+
+test("Mayor que", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test("Verdadero", () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""));
+};
+
+test("Probar un Callback", () => {
+  reverseString("Hola", (str) => {
+    expect(str).toBe("aloH");
+  });
+});
+
+const reverseString2 = (str) => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(error("Error"));
+    }
+    resolve(str.split("").reverse().join(""));
+  });
+};
+
+test("Probar una promesa", () => {
+  return reverseString2("Hola").then((string) => {
+    expect(string).toBe("aloH");
+  });
+});
+
+test("Probar async/await", async () => {
+  const string = await reverseString2("hola");
+  expect(string).toBe("aloh");
+});
+
+afterEach(() => console.log("DEPUÉS DE CADA PRUEBA"));
+afterAll(() => console.log("DESPUÉS DE TODAS LAS PRUEBAS"));
+beforeEach(() => console.log("ANTES DE CADA PRUEBA"));
+beforeAll(() => console.log("ANTES DE TODAS LAS PRUEBAS"));
